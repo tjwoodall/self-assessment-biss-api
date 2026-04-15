462a79a40fe30376056f83c8ffa2d1daad7e3080
jesse1411 <198622734+jesse1411@users.noreply.github.com>
2026-04-15 13:15:42 +0100

    MTDSA-33924 Increase Test Coverage to Meet Minimum Threshold in self-assessment-biss-api (#174)

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 90,
+    ScoverageKeys.coverageMinimumStmtTotal := 95,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
